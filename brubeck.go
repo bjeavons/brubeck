4d2bacee54f0979900d8e72ee88e7e7d5517f827
Ben Jeavons <bjeavons@users.noreply.github.com>
2019-07-30 19:39:51 -0700

    Extract func, time fwd, days, months, years

--- brubeck.go
+++ brubeck.go
@@ -5,6 +5,7 @@ import (
   "os"
   "time"
   "strconv"
+  "errors"
 )
 
 func main() {
@@ -19,18 +20,33 @@ func main() {
     }
     tm := time.Unix(timestamp, 0)
     fmt.Println(tm)
-  } else if argCount == 3 && os.Args[3] == "ago" {
-    delta, err := strconv.Atoi(os.Args[1])
+  } else if argCount == 3 && (os.Args[3] == "ago" || os.Args[3] == "later") {
+    amt, err := strconv.Atoi(os.Args[1])
     if err != nil {
-        fmt.Println(err)
-        os.Exit(2)
+        panic(err)
     }
-    start := time.Now()
-    switch os.Args[2] {
-    case "week":
-      before := start.AddDate(0, 0, -delta * 7)
-      // moving print out or switch results in undefined before
-      fmt.Println(before.Unix())
+    if os.Args[3] == "ago" {
+      amt = -amt
     }
+    tm, err := time_change(amt, os.Args[2])
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(tm)
   }
+}
+
+func time_change(amt int, unit string) (int64, error) {
+  start := time.Now()
+  switch unit {
+  case "day", "days", "d":
+    return start.AddDate(0, 0, amt).Unix(), nil
+  case "week", "weeks", "w":
+    return start.AddDate(0, 0, amt * 7).Unix(), nil
+  case "month", "months", "m":
+    return start.AddDate(0, amt, 0).Unix(), nil
+  case "year", "years", "y":
+    return start.AddDate(amt, 0, 0).Unix(), nil
+  }
+  return -1, errors.New("Not implemented")
 }
