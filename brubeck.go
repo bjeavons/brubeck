37aafb07a74d2976484fb21154b9783730d32640
Ben Jeavons <bjeavons@users.noreply.github.com>
2019-07-31 22:46:28 -0700

    Timezone conversion, return time

--- brubeck.go
+++ brubeck.go
@@ -6,6 +6,7 @@ import (
   "time"
   "strconv"
   "errors"
+  "strings"
 )
 
 func main() {
@@ -20,6 +21,16 @@ func main() {
     }
     tm := time.Unix(timestamp, 0)
     fmt.Println(tm)
+  } else if argCount == 3 && os.Args[2] == "in" {
+    timestamp, err := strconv.ParseInt(os.Args[1], 10, 64)
+    if err != nil {
+        panic(err)
+    }
+    tm, err := time_convert(timestamp, os.Args[3])
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(tm)
   } else if argCount == 3 && (os.Args[3] == "ago" || os.Args[3] == "later") {
     amt, err := strconv.Atoi(os.Args[1])
     if err != nil {
@@ -32,21 +43,41 @@ func main() {
     if err != nil {
         panic(err)
     }
-    fmt.Println(tm)
+    fmt.Println(tm.Unix())
   }
 }
 
-func time_change(amt int, unit string) (int64, error) {
+func time_change(amt int, unit string) (time.Time, error) {
   start := time.Now()
   switch unit {
   case "day", "days", "d":
-    return start.AddDate(0, 0, amt).Unix(), nil
+    return start.AddDate(0, 0, amt), nil
   case "week", "weeks", "w":
-    return start.AddDate(0, 0, amt * 7).Unix(), nil
+    return start.AddDate(0, 0, amt * 7), nil
   case "month", "months", "m":
-    return start.AddDate(0, amt, 0).Unix(), nil
+    return start.AddDate(0, amt, 0), nil
   case "year", "years", "y":
-    return start.AddDate(amt, 0, 0).Unix(), nil
+    return start.AddDate(amt, 0, 0), nil
   }
-  return -1, errors.New("Not implemented")
+  return start, errors.New("Not implemented")
+}
+
+func time_convert(timestamp int64, abr string) (time.Time, error) {
+  var loc string
+  switch strings.ToLower(abr) {
+  case "pst", "pdt":
+    loc = "America/Los_Angeles"
+  case "mst", "mdt":
+    loc = "America/Denver"
+  case "cst", "cdt":
+    loc = "America/Chicago"
+  case "est", "edt":
+    loc = "America/New_York"
+  }
+  location, err := time.LoadLocation(loc)
+  if err != nil {
+      panic(err)
+  }
+  tm := time.Unix(timestamp, 0)
+  return tm.In(location), nil
 }
